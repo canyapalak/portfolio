3e83b7e6a44a3d1d099daf02af0da8b87480aeac
canyapalak <canyapalak@gmail.com>
2023-03-18 00:08:41 +0100

    🔥 ready to deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,5 @@
 import Head from "next/head";
-import {
-  BsFillMoonStarsFill,
-  BsGithub,
-  BsLinkedin,
-  BsMailbox,
-} from "react-icons/bs";
+import { BsFillMoonFill, BsFillSunFill, BsMailbox } from "react-icons/bs";
 import {
   SiHtml5,
   SiCss3,
@@ -21,6 +16,7 @@ import {
   SiFigma,
   SiPostman,
 } from "react-icons/si";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import Image from "next/image";
 import Avatar from "../assets/nobg-avatar.png";
 import { projects } from "@/data/skills-and-projects";
@@ -35,22 +31,32 @@ export default function Home() {
         <title>Can Yapalak Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gray-50 px-10 dark:bg-gray-900">
-        <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-around">
+      <main>
+        {/* <main className="bg-gray-50 px-10 dark:bg-gray-900"> */}
+        <section className="min-h-screen bg-gray-100 px-10 dark:bg-gray-800">
+          <nav className="py-10 mb-5 flex justify-around">
             <h1 className="text-2xl font-cocogoose dark:text-gray-200">
               portfolio
             </h1>
             <ul className="flex items-center gap-5">
               <li>
-                <BsFillMoonStarsFill
-                  onClick={() => setDarkMode(!darkMode)}
-                  className="cursor-pointer text-2xl dark:text-gray-200"
-                />
+                {!darkMode ? (
+                  <BsFillMoonFill
+                    onClick={() => setDarkMode(!darkMode)}
+                    className="cursor-pointer text-2xl"
+                  />
+                ) : (
+                  <BsFillSunFill
+                    onClick={() => setDarkMode(!darkMode)}
+                    className="cursor-pointer text-2xl dark:text-gray-200"
+                  />
+                )}
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md font-cocogooseprolight text-sm"
+                  className="bg-gradient-to-r from-cyan-500 hover:from-cyan-600 to-teal-500
+                   hover:to-teal-600 text-white px-4 py-2 
+                  rounded-md font-cocogooseprolight text-sm"
                   href="#"
                 >
                   Resume
@@ -58,7 +64,7 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="text-center py-5 px-10 font-cocogooseprolight">
+          <div className="text-center py-5 px-5 font-cocogooseprolight">
             <div
               className="relative mx-auto bg-gradient-to-b
           from-slate-500 rounded-full w-60 h-60 overflow-hidden
@@ -77,33 +83,41 @@ export default function Home() {
             <h3 className="text-xl py-2 dark:text-gray-200">
               Junior Web Developer
             </h3>
-            <p className="text-sm py-2 dark:text-gray-200">
+            <p className="text-sm py-2 dark:text-gray-200 max-w-[1000px] text-center mx-auto">
               I'm Can, a fresh Junior Web Developer residing in Berlin, Germany.
-              <br></br>
               I'm currently learning how to be a Full-Stack Web Dev and spending
-              all my time and energy in this new field of expertise.
-              <br></br>My purpose is to work as a full-time web developer and
-              keep on progressing with experience.
+              all my time and energy in this new field of expertise. My purpose
+              is to work as a full-time web developer and keep on progressing
+              with experience.
             </p>
           </div>
-          <div className="text-3xl flex justify-center gap-7 text-cyan-600 mb-10">
-            <a target="_blank" href="https://github.com/canyapalak">
-              <BsGithub />
+          <div className="text-4xl flex justify-center gap-7 text-cyan-600 pb-10">
+            <a
+              target="_blank"
+              href="https://github.com/canyapalak"
+              className=" hover:text-cyan-800"
+            >
+              <AiFillGithub />
             </a>
             <a
               target="_blank"
               href="https://www.linkedin.com/in/can-yapalak-a1359b76/"
+              className=" hover:text-cyan-800"
             >
-              <BsLinkedin className="cursor-pointer" />
+              <AiFillLinkedin className="cursor-pointer" />
             </a>
-            <a target="_blank" href="mailto:canyapalak@gmail.com">
+            <a
+              target="_blank"
+              href="mailto:canyapalak@gmail.com"
+              className=" hover:text-cyan-800"
+            >
               <BsMailbox className="cursor-pointer" />
             </a>
           </div>
         </section>
-        <section className="mt-5 min-h-screen">
-          <div className="font-cocogooseprolight max-w-40 pt-40 dark:text-gray-200">
-            <h3 className="text-xl text-center pb-2 mb-4">My Skills</h3>
+        <section className="bg-orange-100 min-h-screen px-10 dark:bg-slate-900">
+          <div className="font-cocogooseprolight max-w-40 pt-10 dark:text-gray-200">
+            <h3 className="text-xl text-center pb-2 mb-4">Skills</h3>
             <p className="text-sm px-5 pb-10 text-center max-w-screen-lg mx-auto ">
               This is my toolbox for now. I tend to learn a new tool in every
               new project and gain more experience on what I have learned
@@ -113,60 +127,60 @@ export default function Home() {
             </p>
           </div>
           <div
-            className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 text-center
+            className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 text-center
           gap-5 p-10 rounded-lg bg-gradient-to-b
-          from-teal-300 shadow-md mb-10 max-w-screen-sm mx-auto "
+          from-stone-400 to-stone-50 shadow-md mb-10 max-w-screen-sm mx-auto "
           >
             <a
               target="_blank"
               href="https://www.w3schools.com/html/default.asp"
             >
-              <SiHtml5 className="text-6xl mx-auto" />
+              <SiHtml5 className="text-6xl mx-auto  hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://www.w3schools.com/css/default.asp">
-              <SiCss3 className="text-6xl mx-auto" />
+              <SiCss3 className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://www.javascript.com/">
-              <SiJavascript className="text-6xl mx-auto" />
+              <SiJavascript className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://reactjs.org/">
-              <SiReact className="text-6xl mx-auto" />
+              <SiReact className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://getbootstrap.com/">
-              <SiBootstrap className="text-6xl mx-auto" />
+              <SiBootstrap className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a href="https://mui.com/">
-              <SiMui className="text-6xl mx-auto" />
+              <SiMui className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://tailwindcss.com/">
-              <SiTailwindcss className="text-6xl mx-auto" />
+              <SiTailwindcss className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://firebase.google.com/">
-              <SiFirebase className="text-6xl mx-auto" />
+              <SiFirebase className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://nodejs.org/en/">
-              <SiNodedotjs className="text-6xl mx-auto" />
+              <SiNodedotjs className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://www.mongodb.com/">
-              <SiMongodb className="text-6xl mx-auto" />
+              <SiMongodb className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://expressjs.com/">
-              <SiExpress className="text-6xl mx-auto" />
+              <SiExpress className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://nextjs.org/">
-              <SiNextdotjs className="text-6xl mx-auto" />
+              <SiNextdotjs className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://www.figma.com/">
-              <SiFigma className="text-6xl mx-auto" />
+              <SiFigma className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
             <a target="_blank" href="https://www.postman.com/">
-              <SiPostman className="text-6xl mx-auto" />
+              <SiPostman className="text-6xl mx-auto hover:text-cyan-800" />
             </a>
           </div>
         </section>
-        <section className="min-h-screen">
-          <div className="font-cocogooseprolight max-w-40 pt-40 text-center dark:text-gray-200">
-            <h3 className="text-xl pb-2 mb-5">Previous Projects</h3>
+        <section className="min-h-screen px-10 bg-teal-50 dark:bg-zinc-800">
+          <div className="font-cocogooseprolight max-w-40 pt-10 text-center dark:text-gray-200">
+            <h3 className="text-xl pb-2 mb-5">Projects</h3>
             <p className="text-sm px-5 pb-10 max-w-screen-lg mx-auto">
               These are the projects I have made so far. They are all
               single-person projects, which means that both coding and styling
@@ -176,14 +190,20 @@ export default function Home() {
               full-stack multi-functional ones.
             </p>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:mx-5 gap-10 mb-5 2xl:mx-40 md:mx-10 sm:mx-10 ">
+          <div
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:mx-5 gap-10 pb-5 2xl:mx-40 
+          md:mx-10 sm:mx-10 "
+          >
             {projects.map((project) => {
               return (
                 <article
                   key={project.id}
                   className="basis-1/4 flex-1 group overflow-hidden relative font-cocogooseprolight"
                 >
-                  <div className="z-50 absolute bg-black/80 h-full flex flex-col gap-5 justify-center items-center px-5 rounded-lg translate-y-full group-hover:translate-y-0 text-white duration-300">
+                  <div
+                    className="z-50 absolute bg-black/80 h-full flex flex-col gap-5 justify-center 
+                  items-center px-5 rounded-lg translate-y-full group-hover:translate-y-0 text-white duration-300"
+                  >
                     <div>
                       <h2 className="text-xl font-bold text-white text-center text-md pb-3">
                         {project.title}
@@ -196,14 +216,16 @@ export default function Home() {
                       <a
                         href={project.pathLive}
                         target="_blank"
-                        className="rounded px-6 py-2 flex justify-center items-center bg-gradient-to-r from-cyan-500 to-teal-300 text-sm"
+                        className="rounded px-6 py-2 flex justify-center items-center 
+                        bg-gradient-to-r from-cyan-500 to-teal-300 text-sm"
                       >
                         Live
                       </a>
                       <a
                         href={project.pathRepo}
                         target="_blank"
-                        className="rounded px-6 py-2 flex justify-center items-center bg-gradient-to-r from-emerald-500 to-green-300 text-sm"
+                        className="rounded px-6 py-2 flex justify-center items-center 
+                        bg-gradient-to-r from-emerald-500 to-green-300 text-sm"
                       >
                         Code
                       </a>
