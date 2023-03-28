b4c2e63f64ad2c7432bdddfd575a08795562cd2b
canyapalak <canyapalak@gmail.com>
2023-03-28 12:42:53 +0200

    initial commit

---
+++ app/head.jsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Can Yapalak Portfolio</title>
+      <meta
+        name="description"
+        content="Made by Can Yapalak using Next.js"
+      ></meta>
+      <link rel="icon" href="/favicon.ico" />
+      <link href="/page.module.css" rel="stylesheet"></link>
+    </>
+  );
+}
