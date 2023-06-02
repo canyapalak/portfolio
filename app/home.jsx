2c6946f570de4f4d3d8289a98a25a1f38bdffb96
canyapalak <canyapalak@gmail.com>
2023-06-02 12:44:43 +0200

    📝 cv update

--- app/home.jsx
+++ app/home.jsx
@@ -58,7 +58,7 @@ export default function Home() {
             </li>
             <li>
               <a
-                href="https://drive.google.com/file/d/124x93YDobOoBoSEs0jPgDn0Ojj5nKmAK/"
+                href="https://drive.google.com/file/d/1LhJqsT4bU9D9aUA6Jg36XgQ2LOHZaQGm/"
                 target="_blank"
                 className="bg-gradient-to-r from-cyan-500 hover:from-cyan-600 to-teal-500
                    hover:to-teal-600 text-white px-4 py-2 
